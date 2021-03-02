9dbbb788f1bc66a058e56568da76f958107cfadb
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-03-02 10:23:29 +0100

    Simple Alert Installed

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,13 +2,18 @@
   <div id="navbar">
     <nav>I'm the navbar</nav>
      <font-awesome-icon icon="spinner" />
-     <button class="btn btn-primary">Click Me</button>
+     <button @click="click" class="btn btn-primary">Click Me</button>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Navbar'
+  name: 'Navbar',
+  methods: {
+    click () {
+      this.$alert('Hello Vue Simple Alert.')
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,31 +2,37 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+// FontAwesome
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faSpinner } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+// Bootstrap
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Simple Alert
+import VueSimpleAlert from 'vue-simple-alert'
+// Axios
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+// Vuex
 import Vuex from 'vuex'
+// Router
 import router from './router'
 
 // Font Awesome
 library.add(faSpinner)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin)
-
 // Axios
 Vue.use(VueAxios, axios)
-
 // Vuex
 Vue.use(Vuex)
+// Simple Alert
+Vue.use(VueSimpleAlert)
 
 Vue.config.productionTip = false
 
