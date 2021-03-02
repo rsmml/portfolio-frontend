cf245b70037e1ad13884bd01016838b01d612e09
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2021-03-02 11:05:51 +0100

    Baffle.js Installed:

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="welcome">
     <h1>{{ msg }}</h1>
+    <h2 class="data">I'm a Baffle.js test text</h2>
     <h2>What's on it:</h2>
     <ul>
       <li>A Simple Navbar</li>
@@ -8,17 +9,31 @@
       <li>Bootstrap</li>
       <li>Axios</li>
       <li>Vuex</li>
+      <li>Animated.css</li>
+      <li>Simple Alert</li>
+      <li>Baffle.js</li>
     </ul>
   </div>
 </template>
 
 <script>
+import baffle from 'baffle'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to this Vue.js Template'
     }
+  },
+  mounted () {
+    // on mounted property start obfuscating
+    let b = baffle('.data', {
+      characters: '>▒█ <▓█▓/ ▓█>▒█ <<▓ ░█▓▒░ ▓░█░ <█▓ ░█▒█ ░▓▓▒',
+      speed: 150
+    })
+    b.start()
+    b.reveal(4000)
   }
 }
 </script>
